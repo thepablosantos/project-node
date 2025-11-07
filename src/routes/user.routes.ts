79ee0c91c993df68383e4104ec0a1e80914c4e4e
thepablosantos <iphonepablosantos@gmail.com>
2025-11-07 16:20:32 +0100

    feat: adiciona rotas de autenticação de usuário (sign-up e sign-in)

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,63 @@
+import {Router} from 'express';
+import { pool } from '../mysql';
+import { v4 as uuidv4 } from 'uuid';
+import{hash, compare} from 'bcrypt';
+import {sign} from 'jsonwebtoken';
+
+const userRoutes = Router();
+
+userRoutes.post('/sign-up', (request, response) => {
+    const{name, email, password} = request.body;
+    pool.getConnection((err: any, connection: any) => {
+      hash(password, 10, (err, hash) => {
+        if (err) {
+          return response.status(500).json(err)
+        }
+  
+        connection.query(
+          'INSERT INTO users (user_id, name, email, password) VALUES (?, ?, ?, ?)',
+          [uuidv4(), name, email, hash],
+          (error: any, results: any, fields: any) => {
+            connection.release();
+            if (error) {
+              return response.status(400).json(error)
+            }
+            return response.status(200).json({success: true});
+          }
+        )
+      })
+    })
+})
+
+userRoutes.post('/sign-in', (request, response) => {
+    const{name, email, password} = request.body;
+    pool.getConnection((err: any, connection: any) => {
+  
+        connection.query(
+          'SELECT * FROM users WHERE email = ?',
+          [email],
+          (error: any, results: any, fields: any) => {
+            connection.release();
+            if (error) {
+              return response.status(400).json({error: 'Authentication failed'})
+            }
+            compare(password, results[0].password, (err, result) =>{
+              if (err) {
+                return response.status(400).json({error: 'Authentication failed'})
+              }
+  
+              if(result) {
+                // Gerar token JWT
+                const token = sign({
+                  id: results[0].user_id,
+                  email: results[0].email,
+                }, 'secret', {expiresIn: '1h'})
+                return response.status(200).json({token: token})
+              }
+            })
+          }
+        )
+    })
+  })
+
+export {userRoutes};
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,8 @@
 import express, { Request, Response } from 'express';
-import { pool } from './mysql';
-import { v4 as uuidv4 } from 'uuid';
+import { userRoutes } from './routes/user.routes';
 const app = express();
 
 app.use(express.json());
-
-app.post('/user', (request, response) => {
-  const{name, email, password} = request.body;
-  pool.getConnection((err: any, connection: any) => {
-    connection.query(
-      'INSERT INTO users (user_id, name, email, password) VALUES (?, ?, ?, ?)',
-      [uuidv4(), name, email, password],
-      ( error: any, results: any, fields: any) => {
-        if (error) {
-          return response.status(400).json(error)
-        }
-        return response.status(200).json({success: true});
-      }
-    )
-  })
-})
+app.use('/user', userRoutes);
 
 app.listen(3000, () => console.log('Servidor backend ativo na porta 3000'));
